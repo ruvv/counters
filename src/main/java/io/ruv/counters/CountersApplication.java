193cf8ca7a26f62fb6ae8b849787afce730296f0
ruv <brand20@yandex.ru>
2022-11-22 16:59:58 +0300

    added dummy spring project, added .gitignore

---
+++ src/main/java/io/ruv/counters/CountersApplication.java
@@ -0,0 +1,13 @@
+package io.ruv.counters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountersApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CountersApplication.class, args);
+    }
+}
---
+++ src/test/java/io/ruv/counters/CountersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ruv.counters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
